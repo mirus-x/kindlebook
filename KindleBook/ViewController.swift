6cf0a453084030fc372c6d627a3d54802c8264eb
mirus <developer@2x2.pw>
2018-11-05 17:42:36 -0600

    Initial Commit

---
+++ KindleBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KindleBook
+//
+//  Created by 2x2 on 11/5/18.
+//  Copyright © 2018 2x2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
