e32ebcc5a5fa072e7350c7f7dd49a0e6e8305624
mirus <developer@2x2.pw>
2018-11-05 18:44:40 -0600

    Created Book And Pages class to sustain all information about book Finished 1 lesson in this series

--- KindleBook/ViewController.swift
+++ KindleBook/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
+class Page{
+    let number: Int
+    let text: String
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
+
+class Book{
+    let title: String
+    let author: String
+    let pages: [Page]
+    
+    init(title: String, author: String, pages:[Page]) {
+        self.title = title
+        self.author = author
+        self.pages = pages
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = .purple
+        let page1 = Page(number: 1, text: "This is first page of the book")
+        let page2 = Page(number: 2, text: "This is second page of the book")
+        let page3 = Page(number: 3, text: "This is third page of the book")
+        
+        let pages = [page1, page2, page3]
+        
+        let book = Book(title: "Incredible title of the first book",
+                        author: "Incredible Writer",
+                        pages: pages)
+        print("Title: \(book.title) \n Author: \(book.author)" )
+        
     }
 
 
